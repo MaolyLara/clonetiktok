1b2ae3a5ea9d92df72fc48f3372bc8f0e5540dce
MaolyLara <85379351+MaolyLara@users.noreply.github.com>
2023-03-30 19:54:58 -0300

    Aula 02

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,18 +1,31 @@
-import React from 'react'
-import "./video.css"
+import React, { useRef, useState } from "react";
+import "./video.css";
 
 function Video() {
-    return (
-        <div className='video'>
-            TikTok
-            <video 
-                className='video__player'
-                src='https://v16-webapp-prime.tiktok.com/video/tos/useast2a/tos-useast2a-ve-0068c002/o8ZWQGqD0VRfIUOIkBejA2nnQgJAXB7qbt1E3L/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=806&bt=403&cs=0&ds=3&ft=_RwJrBiTq8ZmoL.cCc_vjyBM8AhLrus&mime_type=video_mp4&qs=0&rc=PGQ7OjY8aDc5PDtpNTc7NEBpM3N5bDg6ZjdnaTMzNzczM0A1NWFjNjQ2Ni4xYmJeXjQwYSNkYmtlcjQwaV9gLS1kMTZzcw%3D%3D&btag=80000&expire=1680233841&l=20230330213709AE3C1477ED2E1C684012&ply_type=2&policy=2&signature=65b15bf18818a16420f85fa321268f28&tk=tt_chain_token'
-            >
+    const videoRef = useRef(null);
+    const [play, setPlay] = useState(false);
 
+    function handdleStart() {
+        if (play) {
+        videoRef.current.pause();
+        setPlay(false);
+        } else {
+        videoRef.current.play();
+        setPlay(true);
+        }
+    }
+
+    return (
+        <div className="video">
+            <video
+                className="video__player"
+                ref={videoRef}
+                onClick={handdleStart}
+                loop
+                src="https://firebasestorage.googleapis.com/v0/b/jornadadevtr.appspot.com/o/WhatsApp%20Video%202023-03-29%20at%2020.04.52.mp4?alt=media&token=db4be029-27cc-4af9-b75d-3ce5fd1b1fe2">
             </video>
         </div>
-    )
+    );
 }
 
-export default Video
+export default Video;
