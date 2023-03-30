8781315c38c76adca7f30d396ce774fe8ec3a921
MaolyLara <85379351+MaolyLara@users.noreply.github.com>
2023-03-30 18:07:38 -0300

    Aula 2

---
+++ src/pages/Video.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./video.css"
+
+function Video() {
+    return (
+        <div className='video'>
+            Sou um componente Videos
+            <video className='video__player'>
+
+            </video>
+        </div>
+    )
+}
+
+export default Video
